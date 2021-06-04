0b683039050d08fadfbcbdff436a729875aae595
Josebias Neto <josebiasbsneto@gmail.com>
2021-06-03 21:34:46 -0300

    Revert "Add two lines"
    This reverts commit 3f5c34e01694c55fd2a80804da94ff16edaf9b04.

--- app/src/main/java/com/example/cronometro/MainActivity.kt
+++ app/src/main/java/com/example/cronometro/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        
         binding.iniciar.setOnClickListener{
             IniciarCronometro()
     }
